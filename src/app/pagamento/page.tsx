48447c196fc8bd65f26dda0d6a92993bcfdada7a
victorrdias <vi_dias_15@hotmail.com>
2023-05-12 20:33:15 -0300

    fix: header and image position

--- src/app/pagamento/page.tsx
+++ src/app/pagamento/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Header from "@/components/header/Header";
 import Cart from "@/components/paymentScreen/cart";
 import Details from "@/components/paymentScreen/details";
 import { Container, Flex } from "@chakra-ui/react";
@@ -7,6 +8,7 @@ import { Container, Flex } from "@chakra-ui/react";
 export default function Payment() {
   return (
     <Container maxWidth="container.xl" p="0">
+      <Header />
       <Flex
         direction={{ base: "column-reverse", md: "row" }}
         h={{ base: "auto", md: "100vh" }}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
 import Subhero from "@/components/subhero/Subhero";
 import { Main } from "@/layout/Main";
